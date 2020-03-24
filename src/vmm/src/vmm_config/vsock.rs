0be32796178a1e9ef1d42296dee3031ae9171e5f
Adrian Catangiu <acatan@amazon.com>
2020-03-24 22:10:03 +0200

    vmm: Create Vsock during config instead of boot
    Following the more decoupled design we can now create the vsock device
    on its configuration path, rather than saving a config and delaying
    creation to boot-time.
    
    This model allows user-errors (like invalid resources or permissions)
    to be reported as part of the configuration step rather than later
    at attempted boot.
    It also removes the time window between configuration and boot where
    changes to the host system (creating sockets, changing permissions,
    etc) could cause microVM boot failures.
    
    Signed-off-by: Adrian Catangiu <acatan@amazon.com>

--- src/vmm/src/vmm_config/vsock.rs
+++ src/vmm/src/vmm_config/vsock.rs
@@ -1,6 +1,34 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::fmt;
+use std::sync::{Arc, Mutex};
+
+use devices::virtio::{Vsock, VsockError, VsockUnixBackend, VsockUnixBackendError};
+
+/// Errors associated with `NetworkInterfaceConfig`.
+#[derive(Debug)]
+pub enum VsockConfigError {
+    /// Failed to create the backend for the vsock device.
+    CreateVsockBackend(VsockUnixBackendError),
+    /// Failed to create the vsock device.
+    CreateVsockDevice(VsockError),
+}
+
+impl fmt::Display for VsockConfigError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::VsockConfigError::*;
+        match *self {
+            CreateVsockBackend(ref e) => {
+                write!(f, "Cannot create backend for vsock device: {:?}", e)
+            }
+            CreateVsockDevice(ref e) => write!(f, "Cannot create vsock device: {:?}", e),
+        }
+    }
+}
+
+type Result<T> = std::result::Result<T, VsockConfigError>;
+
 /// This struct represents the strongly typed equivalent of the json body
 /// from vsock related requests.
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
@@ -12,4 +40,139 @@ pub struct VsockDeviceConfig {
     pub guest_cid: u32,
     /// Path to local unix socket.
     pub uds_path: String,
+}
+
+struct VsockAndUnixPath {
+    vsock: Arc<Mutex<Vsock<VsockUnixBackend>>>,
+    uds_path: String,
+}
+
+/// A builder of Vsock with Unix backend from 'VsockDeviceConfig'.
+#[derive(Default)]
+pub struct VsockStore {
+    inner: Option<VsockAndUnixPath>,
+}
+
+impl VsockStore {
+    /// Creates an empty Vsock with Unix backend Store.
+    pub fn new() -> Self {
+        Self { inner: None }
+    }
+
+    /// Inserts a Unix backend Vsock in the store.
+    /// If an entry already exists, it will overwrite it.
+    pub fn insert(&mut self, cfg: VsockDeviceConfig) -> Result<()> {
+        // Make sure to drop the old one and remove the socket before creating a new one.
+        if let Some(existing) = self.inner.take() {
+            let _ = std::fs::remove_file(existing.uds_path);
+        }
+        self.inner = Some(VsockAndUnixPath {
+            uds_path: cfg.uds_path.clone(),
+            vsock: Arc::new(Mutex::new(Self::create_unixsock_vsock(cfg)?)),
+        });
+        Ok(())
+    }
+
+    /// Provides a reference to the Vsock if present.
+    pub fn get(&self) -> Option<&Arc<Mutex<Vsock<VsockUnixBackend>>>> {
+        self.inner.as_ref().map(|pair| &pair.vsock)
+    }
+
+    /// Creates a Vsock device from a VsockDeviceConfig.
+    pub fn create_unixsock_vsock(cfg: VsockDeviceConfig) -> Result<Vsock<VsockUnixBackend>> {
+        let backend = VsockUnixBackend::new(u64::from(cfg.guest_cid), cfg.uds_path)
+            .map_err(VsockConfigError::CreateVsockBackend)?;
+
+        Ok(Vsock::new(cfg.vsock_id, u64::from(cfg.guest_cid), backend)
+            .map_err(VsockConfigError::CreateVsockDevice)?)
+    }
+}
+
+#[cfg(test)]
+pub(crate) mod tests {
+    use super::*;
+    use utils::tempfile::TempFile;
+
+    // Placeholder for the path where a socket file will be created.
+    // The socket file will be removed when the scope ends.
+    pub(crate) struct TempSockFile {
+        path: String,
+    }
+
+    impl TempSockFile {
+        pub fn new(tmp_file: TempFile) -> Self {
+            TempSockFile {
+                path: String::from(tmp_file.as_path().to_str().unwrap()),
+            }
+        }
+        pub fn path(&self) -> &String {
+            &self.path
+        }
+    }
+
+    impl Drop for TempSockFile {
+        fn drop(&mut self) {
+            let _ = std::fs::remove_file(&self.path);
+        }
+    }
+
+    pub(crate) fn default_config(tmp_sock_file: &TempSockFile) -> VsockDeviceConfig {
+        let vsock_dev_id = "vsock_1";
+        VsockDeviceConfig {
+            vsock_id: vsock_dev_id.to_string(),
+            guest_cid: 3,
+            uds_path: tmp_sock_file.path().clone(),
+        }
+    }
+
+    #[test]
+    fn test_vsock_create() {
+        let tmp_sock_file = TempSockFile::new(TempFile::new().unwrap());
+        let vsock_config = default_config(&tmp_sock_file);
+        VsockStore::create_unixsock_vsock(vsock_config).unwrap();
+    }
+
+    #[test]
+    fn test_vsock_insert() {
+        let mut store = VsockStore::new();
+        let tmp_sock_file = TempSockFile::new(TempFile::new().unwrap());
+        let mut vsock_config = default_config(&tmp_sock_file);
+
+        store.insert(vsock_config.clone()).unwrap();
+        let vsock = store.get().unwrap();
+        assert_eq!(vsock.lock().unwrap().id(), &vsock_config.vsock_id);
+
+        let new_id = "another".to_string();
+        vsock_config.vsock_id = new_id.clone();
+        store.insert(vsock_config).unwrap();
+        let vsock = store.get().unwrap();
+        assert_eq!(vsock.lock().unwrap().id(), &new_id);
+    }
+
+    #[test]
+    fn test_error_messages() {
+        use super::VsockConfigError::*;
+        use std::io;
+        let err = CreateVsockBackend(devices::virtio::VsockUnixBackendError::EpollAdd(
+            io::Error::from_raw_os_error(0),
+        ));
+        assert_eq!(
+            format!("{}", err),
+            format!(
+                "Cannot create backend for vsock device: {:?}",
+                devices::virtio::VsockUnixBackendError::EpollAdd(io::Error::from_raw_os_error(0))
+            )
+        );
+
+        let err = CreateVsockDevice(devices::virtio::VsockError::EventFd(
+            io::Error::from_raw_os_error(0),
+        ));
+        assert_eq!(
+            format!("{}", err),
+            format!(
+                "Cannot create vsock device: {:?}",
+                devices::virtio::VsockError::EventFd(io::Error::from_raw_os_error(0))
+            )
+        );
+    }
 }
